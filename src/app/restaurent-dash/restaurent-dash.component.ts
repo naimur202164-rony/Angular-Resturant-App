a07bbbea8d5e0475e86357b1a55349d79b0071af
Your Name <naimur202163@gmail.com>
2022-08-20 09:25:00 +0600

    form value added

--- src/app/restaurent-dash/restaurent-dash.component.ts
+++ src/app/restaurent-dash/restaurent-dash.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurent-dash',
   templateUrl: './restaurent-dash.component.html',
-  styleUrls: ['./restaurent-dash.component.css']
+  styleUrls: ['./restaurent-dash.component.css'],
 })
 export class RestaurentDashComponent implements OnInit {
-
-  constructor() { }
+  formValue!: FormGroup;
+  constructor(private formBulider: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formValue = this.formBulider.group({
+      name: [' '],
+      emai: [''],
+      mobile: [''],
+      address: [''],
+      services: [''],
+    });
   }
-
 }
